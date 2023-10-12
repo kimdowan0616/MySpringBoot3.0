2c38522496657d6aa3a00a9971cd76c1b41c2b86
kimdowan0616 <ourbaby7@nate.com>
2023-10-12 16:48:36 +0900

    spring boot project initial setup

---
+++ src/main/java/com/basic/myspringboot/MySpringBoot3Application.java
@@ -0,0 +1,18 @@
+package com.basic.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MySpringBoot3Application {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(MySpringBoot3Application.class, args);
+		SpringApplication application = new SpringApplication(MySpringBoot3Application.class);
+		// Web Application Type 변경
+		application.setWebApplicationType(WebApplicationType.SERVLET);	// default는 SERVELT이다. NONE으로 하면 웹 어플이 아니라는 의미.
+		application.run(args);
+	}
+}
